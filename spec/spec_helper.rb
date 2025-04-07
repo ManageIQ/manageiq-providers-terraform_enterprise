8bb9ca502704727fc6faf6c16b8b58f9ed8bd502
Adam Grare <adam@grare.com>
2025-04-07 16:55:49 -0400

    Initial provider generation

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+if ENV['CI']
+  require 'simplecov'
+  SimpleCov.start
+end
+
+Dir[Rails.root.join("spec/shared/**/*.rb")].sort.each { |f| require f }
+Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
+
+require "manageiq/providers/terraform_enterprise"
+
+VCR.configure do |config|
+  config.ignore_hosts 'codeclimate.com' if ENV['CI']
+  config.cassette_library_dir = File.join(ManageIQ::Providers::TerraformEnterprise::Engine.root, 'spec/vcr_cassettes')
+end
