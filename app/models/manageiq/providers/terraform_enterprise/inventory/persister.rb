0dfb6be8f1a97f2e28cc9250d41567294915acdf
Adam Grare <adam@grare.com>
2025-05-08 11:44:29 -0400

    Workspaces should be ConfigurationScripts not Payloads

--- app/models/manageiq/providers/terraform_enterprise/inventory/persister.rb
+++ app/models/manageiq/providers/terraform_enterprise/inventory/persister.rb
@@ -2,9 +2,6 @@ class ManageIQ::Providers::TerraformEnterprise::Inventory::Persister < ManageIQ:
   def initialize_inventory_collections
     add_collection(automation, :configuration_scripts)
     add_collection(automation, :configuration_script_sources)
-    add_collection(automation, :configuration_script_payloads) do |builder|
-      builder.add_properties(:manager_ref => %i[manager_ref])
-      builder.add_default_values(:manager => manager)
-    end
+    add_collection(automation, :configuration_script_payloads)
   end
 end
