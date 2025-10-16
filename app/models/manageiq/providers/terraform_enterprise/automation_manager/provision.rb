397c5fdd7e311300015c40ca0b8626700b810d26
Adam Grare <adam@grare.com>
2025-10-16 13:00:08 -0400

    Use base AutomationManager::Provision class

--- app/models/manageiq/providers/terraform_enterprise/automation_manager/provision.rb
+++ app/models/manageiq/providers/terraform_enterprise/automation_manager/provision.rb
@@ -1,17 +1,5 @@
-class ManageIQ::Providers::TerraformEnterprise::AutomationManager::Provision < MiqProvisionTask
+class ManageIQ::Providers::TerraformEnterprise::AutomationManager::Provision < ManageIQ::Providers::AutomationManager::Provision
   include StateMachine
 
   TASK_DESCRIPTION = N_("Terraform Enterprise Workspace Provision")
-
-  def self.request_class
-    MiqProvisionConfigurationScriptRequest
-  end
-
-  def my_role(*)
-    "ems_operations"
-  end
-
-  def my_queue_name
-    source.manager&.queue_name_for_ems_operations
-  end
 end
