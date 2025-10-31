031c807838c74b85174697107c9852894fd81ab8
Adam Grare <adam@grare.com>
2025-10-31 12:19:30 -0400

    Add manager_name for catalog UI

--- app/models/manageiq/providers/terraform_enterprise/automation_manager/provision_workflow.rb
+++ app/models/manageiq/providers/terraform_enterprise/automation_manager/provision_workflow.rb
@@ -6,7 +6,7 @@ class ManageIQ::Providers::TerraformEnterprise::AutomationManager::ProvisionWork
 
   def allowed_configuration_scripts(*args)
     ManageIQ::Providers::TerraformEnterprise::AutomationManager::ConfigurationScript.all.map do |cs|
-      build_ci_hash_struct(cs, %w[name description])
+      build_ci_hash_struct(cs, %w[name description manager_name])
     end
   end
 end
